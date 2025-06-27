78fe3c0778b795413df254848416e20a57057b1d
Abdullah Sherif Mohamed Reda Eldahrawy <eldahrawy@Abdullahs-MacBook-Pro.local>
2025-06-27 04:26:18 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+const axios = require("axios");
+const cron = require("node-cron");
+
+const DATA_FILE = "./lastData.json";
+const PHONE = "201002601277"; // Replace with your WhatsApp number
+const API_KEY = "5264175"; // From CallMeBot
+
+// Step 1: Load old data
+function loadLastData() {
+  if (!fs.existsSync(DATA_FILE)) return null;
+  const raw = fs.readFileSync(DATA_FILE, "utf-8");
+  return JSON.parse(raw);
+}
+
+// Step 2: Save new data
+function saveData(data) {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+}
+
+// Step 3: Send WhatsApp alert using CallMeBot
+async function sendWhatsApp(message) {
+  const url = `https://api.callmebot.com/whatsapp.php?phone=${PHONE}&text=${encodeURIComponent(
+    message
+  )}&apikey=${API_KEY}`;
+  try {
+    const response = await axios.get(url);
+    console.log("WhatsApp sent:", response.data);
+  } catch (err) {
+    console.error("WhatsApp error:", err.message);
+  }
+}
+
+// Step 4: Monitor function
+async function monitor() {
+  try {
+    console.log("Checking data...");
+
+    const requestBody = {
+      id: 0,
+      jsonrpc: "2.0",
+      method: "call",
+      params: {
+        product_template_id: 340,
+        product_id: 1410,
+        combination: [],
+        add_qty: 1,
+        parent_combination: [],
+      },
+    };
+
+    // Replace with your actual API
+    const res = await axios.post(
+      "https://shop.btcegyptgold.com/website_sale/get_combination_info",
+      requestBody,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const currentData = res.data.result;
+    const lastData = loadLastData();
+    priceDff = Math.abs(currentData.price - lastData?.price) || 0;
+
+    // Define change logic: here, we detect change in `completed`
+    if (!lastData || priceDff > 100) {
+      console.log("Significant change detected")  
+      await sendWhatsApp(
+        `1 Gold Coin BTC Price changed from ${currentData.price} to ${
+          lastData?.price || "N/A"
+        }.`
+      );
+      saveData(currentData);
+    } else {
+      console.log("No significant change detected.");
+    }
+  } catch (error) {
+    console.error("Error fetching or processing data:", error.message);
+  }
+}
+
+// Step 5: Schedule job to run every 5 minutes
+cron.schedule("*/5 * * * *", monitor);
+console.log("Cron job scheduled to run every 5 minutes.");
